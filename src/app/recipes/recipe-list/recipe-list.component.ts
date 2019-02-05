3ec2852b771c206777a8165288b708cf8dfb7ca7
Juan Vivas <juanlvo@gmail.com>
2019-02-05 15:12:18 +0100

    feat: Section 3 Lecture 49

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
-  
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simply test', 'https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto:eco,c_fill,g_auto,w_760/https://storage.googleapis.com/gen-atmedia/3/2018/06/d83512a0fe4e2d338f89ccde0c5de227646921cf.jpeg'),
+    new Recipe('Recipe 2', 'Description recipe 2', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2016/06/prawn-laksa-curry-bowl.jpg')
+  ];
+
   constructor() { }
 
   ngOnInit() {
