0359eced74a426c588de8499dc805970803aefcb
Juan Vivas <juanlvo@gmail.com>
2019-03-11 12:13:09 +0100

    feat: Section 10 Lecture 113 passing ingredients fron recipes to the shopping list (via a service)

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipes.services';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,9 +10,12 @@ import { Recipe } from '../recipe.model';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
   }
 
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
 }
